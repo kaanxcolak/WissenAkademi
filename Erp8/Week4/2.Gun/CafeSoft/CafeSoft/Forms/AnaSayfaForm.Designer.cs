014008a5a2ff6b644baf99fa5a830a9de3f183a1
Kaan Colak <kaancolak3458@gmail.com>
2023-03-14 18:24:20 +0300

    updated

--- Erp8/Week4/2.Gun/CafeSoft/CafeSoft/Forms/AnaSayfaForm.Designer.cs
+++ Erp8/Week4/2.Gun/CafeSoft/CafeSoft/Forms/AnaSayfaForm.Designer.cs
@@ -32,12 +32,12 @@
             flpKatlar = new FlowLayoutPanel();
             tableLayoutPanel2 = new TableLayoutPanel();
             tableLayoutPanel3 = new TableLayoutPanel();
-            tableLayoutPanel4 = new TableLayoutPanel();
-            tableLayoutPanel5 = new TableLayoutPanel();
-            tableLayoutPanel6 = new TableLayoutPanel();
             flpMasalar = new FlowLayoutPanel();
             flpKategoriler = new FlowLayoutPanel();
             flpUrunler = new FlowLayoutPanel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            tableLayoutPanel6 = new TableLayoutPanel();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             tableLayoutPanel3.SuspendLayout();
@@ -53,19 +53,21 @@
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(889, 472);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.Size = new Size(1016, 629);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // flpKatlar
             // 
             flpKatlar.Dock = DockStyle.Fill;
-            flpKatlar.Location = new Point(3, 3);
+            flpKatlar.Location = new Point(3, 4);
+            flpKatlar.Margin = new Padding(3, 4, 3, 4);
             flpKatlar.Name = "flpKatlar";
-            flpKatlar.Size = new Size(188, 466);
+            flpKatlar.Size = new Size(216, 621);
             flpKatlar.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -76,47 +78,77 @@
             tableLayoutPanel2.Controls.Add(tableLayoutPanel3, 0, 0);
             tableLayoutPanel2.Controls.Add(tableLayoutPanel4, 1, 0);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(197, 3);
+            tableLayoutPanel2.Location = new Point(225, 4);
+            tableLayoutPanel2.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel2.Size = new Size(689, 466);
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel2.Size = new Size(788, 621);
             tableLayoutPanel2.TabIndex = 1;
             // 
             // tableLayoutPanel3
             // 
             tableLayoutPanel3.ColumnCount = 1;
             tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 23F));
             tableLayoutPanel3.Controls.Add(flpMasalar, 0, 0);
             tableLayoutPanel3.Controls.Add(flpKategoriler, 0, 1);
             tableLayoutPanel3.Controls.Add(flpUrunler, 0, 2);
             tableLayoutPanel3.Dock = DockStyle.Fill;
-            tableLayoutPanel3.Location = new Point(3, 3);
+            tableLayoutPanel3.Location = new Point(3, 4);
+            tableLayoutPanel3.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel3.Name = "tableLayoutPanel3";
             tableLayoutPanel3.RowCount = 3;
             tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 63F));
             tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 37F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 245F));
-            tableLayoutPanel3.Size = new Size(382, 460);
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 327F));
+            tableLayoutPanel3.Size = new Size(438, 613);
             tableLayoutPanel3.TabIndex = 0;
+            // 
+            // flpMasalar
+            // 
+            flpMasalar.Dock = DockStyle.Fill;
+            flpMasalar.Location = new Point(3, 4);
+            flpMasalar.Margin = new Padding(3, 4, 3, 4);
+            flpMasalar.Name = "flpMasalar";
+            flpMasalar.Size = new Size(432, 172);
+            flpMasalar.TabIndex = 0;
+            // 
+            // flpKategoriler
+            // 
+            flpKategoriler.Dock = DockStyle.Fill;
+            flpKategoriler.Location = new Point(3, 184);
+            flpKategoriler.Margin = new Padding(3, 4, 3, 4);
+            flpKategoriler.Name = "flpKategoriler";
+            flpKategoriler.Size = new Size(432, 97);
+            flpKategoriler.TabIndex = 1;
+            // 
+            // flpUrunler
+            // 
+            flpUrunler.Dock = DockStyle.Fill;
+            flpUrunler.Location = new Point(3, 289);
+            flpUrunler.Margin = new Padding(3, 4, 3, 4);
+            flpUrunler.Name = "flpUrunler";
+            flpUrunler.Size = new Size(432, 320);
+            flpUrunler.TabIndex = 2;
             // 
             // tableLayoutPanel4
             // 
             tableLayoutPanel4.ColumnCount = 1;
             tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 23F));
             tableLayoutPanel4.Controls.Add(tableLayoutPanel5, 0, 0);
             tableLayoutPanel4.Controls.Add(tableLayoutPanel6, 0, 1);
             tableLayoutPanel4.Dock = DockStyle.Fill;
-            tableLayoutPanel4.Location = new Point(391, 3);
+            tableLayoutPanel4.Location = new Point(447, 4);
+            tableLayoutPanel4.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel4.Name = "tableLayoutPanel4";
             tableLayoutPanel4.RowCount = 2;
             tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel4.Size = new Size(295, 460);
+            tableLayoutPanel4.Size = new Size(338, 613);
             tableLayoutPanel4.TabIndex = 1;
             // 
             // tableLayoutPanel5
@@ -124,14 +156,15 @@
             tableLayoutPanel5.ColumnCount = 3;
             tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 104F));
             tableLayoutPanel5.Dock = DockStyle.Fill;
-            tableLayoutPanel5.Location = new Point(3, 3);
+            tableLayoutPanel5.Location = new Point(3, 4);
+            tableLayoutPanel5.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel5.Name = "tableLayoutPanel5";
             tableLayoutPanel5.RowCount = 1;
             tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel5.Size = new Size(289, 224);
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Absolute, 299F));
+            tableLayoutPanel5.Size = new Size(332, 298);
             tableLayoutPanel5.TabIndex = 0;
             // 
             // tableLayoutPanel6
@@ -140,45 +173,21 @@
             tableLayoutPanel6.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel6.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel6.Dock = DockStyle.Fill;
-            tableLayoutPanel6.Location = new Point(3, 233);
+            tableLayoutPanel6.Location = new Point(3, 310);
+            tableLayoutPanel6.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel6.Name = "tableLayoutPanel6";
             tableLayoutPanel6.RowCount = 2;
             tableLayoutPanel6.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel6.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel6.Size = new Size(289, 224);
+            tableLayoutPanel6.Size = new Size(332, 299);
             tableLayoutPanel6.TabIndex = 1;
-            // 
-            // flpMasalar
-            // 
-            flpMasalar.Dock = DockStyle.Fill;
-            flpMasalar.Location = new Point(3, 3);
-            flpMasalar.Name = "flpMasalar";
-            flpMasalar.Size = new Size(376, 129);
-            flpMasalar.TabIndex = 0;
-            // 
-            // flpKategoriler
-            // 
-            flpKategoriler.Dock = DockStyle.Fill;
-            flpKategoriler.Location = new Point(3, 138);
-            flpKategoriler.Name = "flpKategoriler";
-            flpKategoriler.Size = new Size(376, 73);
-            flpKategoriler.TabIndex = 1;
-            // 
-            // flpUrunler
-            // 
-            flpUrunler.Dock = DockStyle.Fill;
-            flpUrunler.Location = new Point(3, 217);
-            flpUrunler.Name = "flpUrunler";
-            flpUrunler.Size = new Size(376, 240);
-            flpUrunler.TabIndex = 2;
             // 
             // AnaSayfaForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(889, 472);
+            ClientSize = new Size(1016, 629);
             Controls.Add(tableLayoutPanel1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "AnaSayfaForm";
             Text = "AnaSayfaForm";
             tableLayoutPanel1.ResumeLayout(false);
--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# WissenAkademi
+﻿Wissen Akademi
+=========================
+Wissen Akademi ile aldığımız C# eğitiminde yaptğımız çalışmalar ve teknik bazı bilgiler.
+
+-------------------------
+
+## Notlar
+
+> **1)** signed int aynı zamanda negatif değerleride tutabiliyorken uint sadece 0 ve pozitif değerleri tutar.
+-------------------------
+> **2)** &(Bitwise and) ve &&(Kısa devre ve) arasındaki fark; & hem bitsel hemde mantıksal bir operatör iken && sadece mantıksal operatördür. & ifadenin her iki tarafını değerlendirirken, && işleci ise son sonucu ifade etmek için ifadenin yalnızca sol tarafını değerlendirir.
+-------------------------
+> **3)** string path = @"c:\new\falan.txt"; 
+>> @ işareti escape sequances kısmını normal karakter olarak algılanmasını sağlıyor.
+-------------------------
+> **4)** Dizi aynı tipteki değişkenleri ya da verileri bir arada tutabildiğimiz bir yapıdır.
+-------------------------
+> **5)** int[] yeniSayilar = sayilar[0..^3];  
+>> 0'dan 3. indexe kadar 3(dahil değil) elemanları yeni diziye aktarır.
+-------------------------
+> **6)** bool xorDeneme = true ^ false; 
+>> true döner. 
+-------------------------
+> **7)** string sonuc = sayi2 % 2 == 0 ? "Çift" : "Tek"; 
+>> Ternary if!
+-------------------------
+> **8)** recursive function -  özyinelemeli yani kendi kendini yineliyor.
+-------------------------
+> **9)** ref-out kullanımı; referans yolu ile metoda parametre olarak geçtiğimiz değişken üzerinde yapılacak bütün değişiklikler orjinal değeri etkileyecektir 
+çünkü artık o metod içerisinde değişkenin bir kopyası değilde RAM'de ki adresi bulunmaktadır. out anahtar sözcüğü iki veya daha fazla geriye döndürmek istediğimizde kullandığımız bir anahtar sözcüktür. ref parametresi de aynı işlemi gerçekleştirebilir.
+-------------------------
+> **10)** property yazımında isim büyük harfle başlar! get okur ve dışarı aktarır(output), set ise üzerinde işlem yapmamızı sağlar(input). 
+-------------------------
+> **11)** Full Property 1adet field'ın encapsule edilmesi ile oluşur.
+-------------------------
+> **12)** public DateTime IslemTarihi { get; private set; } = DateTime.Now;
+>> Auto Property!
+-------------------------
+> **13)** public int Yas => DateTime.Now.Year - this.DogumTarihi.Year;
+>> Readonly property - Sadece okuma
+-------------------------
+> **14)** _Ad.Substring(0, 1).ToUpper() + _ad.Substring(1).ToLower(); 
+>> Substring içindeki 2.parametre 1 adet al anlamında!(0 dan başla 1 adet al).
+-------------------------
+> **15)** Tc kimlik numarası veya telefon int alınmaz. String olarak düşünülmesi gerekir!
+-------------------------
+> **16)** 
+>- 1 nibble = 4 bit
+>- 1 byte = 8 bit
+>- 1 kB = 2^10 byte = 1024 byte
+>- 1 MB = 1024 kB = 2^20 byte = 1048576 byte
+>- 1 GB = 1024 MB = 2^30 byte = 1073741824 byte
+>- 1 TB = 1024 Gb = 2^40 byte = 1099511627776 byte
+>- 1 PB = 1024 TB = 2^50 byte = 1125899906842624 byte
+>- 1 EB = 1024 PB = 2^60 byte = 1152921504606846976 byte
+-------------------------
+> **17)** Bilgisayarın; aynı önceliğe sahip işlemleri gerçekleştirme sırası, soldan sağa (baştan sona) doğrudur.
+-------------------------
+> **18)** Herhangi bir komut kullanmadan büyük-küçük harf dönüşümü yapmak için, ilgili harfin ,ASCII koduna 32 eklenebilir/çıkarılabilir.
+-------------------------
+> **19)** 
+>- Bütün koşullar sağlanması gerektiğinde 'VE',
+>- Koşullardan herhangi birinin sağlanması yeterli olduğunda 'VEYA' ,
+>- Koşulu sağlamayan gerektiğinde ise koşulun başına 'DEĞİL' yazılır.
+-------------------------
+> **20)** Mantıksal işlemlerde 'n' tane koşul/değişken varsa, '2^n' tane farklı durum oluşur.
+-------------------------
+> **21)** Programlarda ne kadar az işlem varsa, o kadar hızlı çalışacaklarını unutmayalım.
+-------------------------
+> **22)** Programlardaki sayısal verilerde herhangi bir taban belirtme simgesi yoksa 10 tabanında(decimal) kabul edilirler.
+-------------------------
+> **23)** üstel belirtme biçimi olan "sayıeüs" veya "sayıEüs" anlamı "sayı.10^üs" tür.
+-------------------------
+> **24)** Programlarda sayısal veriler doğrudan, alfasayısal veriler ise tek/çift tırnak içinde gösterilirler.
+-------------------------
+> **25)** Çarpım değişkenine, başlangıç değeri olarak çarpma işleminin etkisiz elemanı olan 1 atanır.
+-------------------------
+> **26)** enum temelRenkler {kırmızı,yeşil, mavi} ----> enum; numaralandırma/sıralama tiplerinin (kümelerin) tanımlandığı kısımdır.
+-------------------------
+> **27)** string'i int'e dönüştürmek ----> System.Int32.Parse("42");
+-------------------------
+> **28)** string'i int'e dönüştürmek ----> System.Int32.Parse("42");
