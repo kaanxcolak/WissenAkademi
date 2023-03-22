4aff72c4fc75c3858f35c3f9fd48e1df4cc76913
Kaan Colak <kaancolak3458@gmail.com>
2023-03-22 23:04:56 +0300

    updated

---
+++ Erp8/Week4/3.Gun/ConsoleProjeleriyleCSharpaGiris/ConsoleProjeleriyleCSharpaGiris/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleProjeleriyleCSharpaGiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
