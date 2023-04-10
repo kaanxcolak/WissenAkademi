a03bfb77619db695c564e3c7d87e2cfdc6458f75
Kaan Colak <kaancolak3458@gmail.com>
2023-04-10 09:19:27 +0300

    updated

---
+++ Erp8/PhoneBook/PhoneBookUI/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles(); //wwwroot klasörünü görmesi için
+
+app.UseRouting(); // /home/indexe gidebilmesi iiçin
+
+app.UseAuthorization(); //Yetkilendirme için
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); // route' default pattern vermek için
+
+app.Run(); //uygulamayı çalıştırır
