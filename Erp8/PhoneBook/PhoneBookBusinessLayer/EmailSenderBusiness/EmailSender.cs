0026542a4b2c52fe5e9a6c4d9bb3180535a0f185
Kaan Colak <kaancolak3458@gmail.com>
2023-04-11 09:21:52 +0300

    created some business layer and changes

--- Erp8/PhoneBook/PhoneBookBusinessLayer/EmailSenderBusiness/EmailSender.cs
+++ Erp8/PhoneBook/PhoneBookBusinessLayer/EmailSenderBusiness/EmailSender.cs
@@ -23,6 +23,7 @@ namespace PhoneBookBusinessLayer.EmailSenderBusiness
         public string Password => _configuration.GetSection("EmailOptions:Password").Value;
         public string Smtp => _configuration.GetSection("EmailOptions:Smtp").Value;
         public int SmtpPort => Convert.ToInt32(_configuration.GetSection("EmailOptions:SmtpPort").Value);
+        public string CCManagers => _configuration.GetSection("ProjectManagersEmails").Value;
 
         private void MailInfoSet(EmailMessage message,out MailMessage mail, out SmtpClient client)
         {
@@ -37,7 +38,27 @@ namespace PhoneBookBusinessLayer.EmailSenderBusiness
                 {
                     mail.To.Add(item);
                 }
-                //CC ve BCC sonra tamamlarız
+                if (message.CC != null)
+                {
+                    foreach (var item in message.CC)
+                    {
+                        mail.CC.Add(item);
+                    }
+                }
+                if (message.BCC != null)
+                {
+                    foreach (var item in message.BCC)
+                    {
+                        mail.Bcc.Add(item);
+                    }
+                }
+                foreach (var item in CCManagers.Split(","))
+                {
+                    mail.CC.Add(item);
+                }
+                
+
+
 
                 mail.Subject = message.Subject;
                 mail.Body = message.Body;
---
+++ Erp8/PhoneBook/PhoneBookBusinessLayer/InterfacesOfManagers/IPhoneTypeManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneBookEntityLayer.ViewModels;
+
+namespace PhoneBookBusinessLayer.InterfacesOfManagers
+{
+    public interface IPhoneTypeManager:IManager<PhoneTypeViewModel, byte>
+    {
+
+    }
+}
