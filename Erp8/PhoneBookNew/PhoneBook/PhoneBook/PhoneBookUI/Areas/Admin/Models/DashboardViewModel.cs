21ccc965879e5de30f87d66b9025c0863fc614b2
kaanc <kaancolak3458@gmail.com>
2023-04-18 13:56:15 +0300

    updated

---
+++ Erp8/PhoneBookNew/PhoneBook/PhoneBook/PhoneBookUI/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneBookUI.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        //public List<PhoneTypePieViewModel> PhoneTypePieData { get; set; }
+        public Dictionary<string, int> PhoneTypePieData { get; set; }
+    }
+}
