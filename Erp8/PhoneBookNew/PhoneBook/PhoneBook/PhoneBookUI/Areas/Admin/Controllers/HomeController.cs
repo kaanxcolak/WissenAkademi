5c836e6f78d5207fb55f72a6f9757a46912d08a8
kaanc <kaancolak3458@gmail.com>
2023-04-18 13:38:27 +0300

    updated

--- Erp8/PhoneBookNew/PhoneBook/PhoneBook/PhoneBookUI/Areas/Admin/Controllers/HomeController.cs
+++ Erp8/PhoneBookNew/PhoneBook/PhoneBook/PhoneBookUI/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneBookBusinessLayer.InterfacesOfManagers;
+using PhoneBookUI.Areas.Admin.Models;
 
 namespace PhoneBookUI.Areas.Admin.Controllers
 {
@@ -28,21 +29,76 @@ namespace PhoneBookUI.Areas.Admin.Controllers
         {
             //Bu ay sisteme kayıt olan üye sayısı
             DateTime thisMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-            ViewBag.MonthlyMemberCount = _memberManager.GetAll(x=>x.CreatedDate > thisMonth.AddDays(-1)).Data.Count();
+            ViewBag.MonthlyMemberCount = _memberManager.GetAll(x => x.CreatedDate > thisMonth.AddDays(-1)).Data.Count();
 
             //bu ay sisteme eklenen numara sayısı
             ViewBag.MonthlyContactCount = _memberPhoneManager.GetAll(x => x.CreatedDate > thisMonth.AddDays(-1)).Data.Count();
 
-            var members = _memberManager.GetAll().Data;
+            var members = _memberManager.GetAll().Data.OrderBy(x => x.CreatedDate);
             //en son eklenen üyenin adı soyadı
             ViewBag.LastMember = $"{members.LastOrDefault()?.Name} {members.LastOrDefault()?.Surname}";
 
             //Rehbere en son eklenen kişinin adı soyadı
-            var contacts = _memberPhoneManager.GetAll().Data;
+            var contacts = _memberPhoneManager.GetAll().Data.OrderBy(x=>x.CreatedDate);
 
             ViewBag.LastContact = contacts.LastOrDefault()?.FriendNameSurname;
-            //temayı giydirip buraya geri döneceğiz.
             return View();
         }
+
+        [Route("/admin/GetPhoneTypePieData")] //buradaki  admin controller'ın route'ı
+        public JsonResult GetPhoneTypePieData()
+        {
+            try
+            {
+                Dictionary<string, int> model = new Dictionary<string, int>();
+                var data = _memberPhoneManager.GetAll().Data;
+                foreach (var item in data)
+                {
+                    if (model.ContainsKey(item.PhoneType.Name)) // wissen kurs tipinden var mı ?
+                    {
+                        //sayısı 1 arttırsın
+                        model[item.PhoneType.Name] += 1;
+                    }
+                    else
+                    {
+                        model.Add(item.PhoneType.Name, 1);
+                    }
+
+
+                } //foreach bitti
+                
+
+                return Json(new
+                {
+                    isSuccess = true,
+                    message = "Veriler geldi",
+                    types = model.Keys.ToArray(), //bir gönder bakalım
+                    points = model.Values.ToArray()
+                }) ;
+            }
+            catch (Exception ex)
+            {
+                return Json(new { isSuccess = false, message = "Veriler getirilemedi!" });
+
+            }
+        }
+
+        [HttpGet]
+        [Route("m")]
+        public IActionResult MemberIndex()
+        {
+            try
+            {
+                var data = _memberManager.GetAll().Data;
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik hata oldu!" + ex.Message);
+                return View();
+            }
+        }
+
     }
 }
+
