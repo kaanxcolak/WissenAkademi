2d4758a607195b35cfe14302ebf02e8fbb4c130e
Kaan Çolak <kaancolak3458@gmail.com>
2023-10-16 09:08:52 +0300

    added Kisi.cs

---
+++ Erp8/TelefonUygulaması/Kisi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonUygulaması
+{
+    public class Kisi
+    {
+        //Encapsulation
+        private string name, surName, phoneNumber;
+        
+        //Constructor
+        public Kisi(string name, string surName, string phoneNumber)
+        {
+            this.name = name;
+            this.surName = surName;
+            this.phoneNumber = phoneNumber;
+        }
+        //props
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
